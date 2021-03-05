f538c754fbe867d3530a34ff100693a99cf64c0d
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-03-05 16:41:19 +0000

    added test file

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
