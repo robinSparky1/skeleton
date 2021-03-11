0fea317cbda7114daf3911e15d04a4a4ae9cb7c2
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-03-11 15:49:06 +0000

    added code to button

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -14,6 +14,6 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
