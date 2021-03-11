5aac9ac81a5c8989726a44d1a8359ad9e315c78f
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-03-11 13:46:21 +0000

    added Verified and date added properties

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,10 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool AccountVerified { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
--- Testing1/tstCustomer.cs
+++ Testing1/tstCustomer.cs
@@ -11,9 +11,27 @@ namespace Testing1
         public void TestMethod1()
         {
             clsCustomer ACustomer = new clsCustomer();
+            
             Assert.IsNotNull(ACustomer);
         }
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
 
+            Boolean TestData = true;
+            ACustomer.AccountVerified = TestData;
+            Assert.AreEqual(ACustomer.AccountVerified, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            DateTime TestData = DateTime.Now.Date;
+            ACustomer.DateAdded = TestData;
+            Assert.AreEqual(ACustomer.DateAdded, TestData);
+        }
     }
 
 }
