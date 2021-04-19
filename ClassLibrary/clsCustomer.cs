2239051877f697c5322d6371a0e63f5da5e70bb0
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-04-19 15:34:58 +0100

    edited accountno

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,13 +4,21 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
+        private Int32 mAccountNo;
         public bool AccountVerified { get; set; }
         public DateTime DateAdded { get; set; }
         public int Ballance { get; set; }
         public string CustomerName { get; set; }
         public string CustomerEmail { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int AccountNo { get; set; }
+        public int AccountNo {
+            get {
+                return mAccountNo;
+            }
+            set {
+                mAccountNo = value;
+            }
+        }
 
         public bool Find(int AccountNo)
         {
