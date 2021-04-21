f92da552d9cedeba4eff5713370f4547ce08ca3d
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-04-21 20:14:59 +0100

    all of name validated

--- Testing1/tstCustomer.cs
+++ Testing1/tstCustomer.cs
@@ -349,14 +349,29 @@ namespace Testing1
             String Error = "";
             //test data to pass
             string CustomerName = "";
-            CustomerName = CustomerName.PadRight(51, 'a');//this should not trigger an error
+            CustomerName = CustomerName.PadRight(51, 'a');//this should trigger an error
             //invoke method
             Error = ACustomer.Valid(CustomerName, CustomerEmail, DateAdded,
             DateOfBirth, Ballance);
             //see if result is correct
             Assert.AreNotEqual(Error, "");
         }
-
+        [TestMethod]
+        public void CutomerNameMaxExtremeMax()
+        {
+            //create instance of class we want
+            clsCustomer ACustomer = new clsCustomer();
+            //string variable to store error message
+            String Error = "";
+            //test data to pass
+            string CustomerName = "";
+            CustomerName = CustomerName.PadRight(1000, 'a');//this should trigger an error
+            //invoke method
+            Error = ACustomer.Valid(CustomerName, CustomerEmail, DateAdded,
+            DateOfBirth, Ballance);
+            //see if result is correct
+            Assert.AreNotEqual(Error, "");
+        }
     }
 
 
