6e824ef36f905b025c23893ea1b3e4be636c7896
Robert Jennings <P2578104@my365.dmu.ac.uk>
2021-04-23 11:08:20 +0100

    reanames new test unit and added new tests and modified customerCollection to pass tests (unfinished)

--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -5,5 +5,7 @@ namespace ClassLibrary
     public class clsCustomerCollection
     {
         public List<clsCustomer> CustomerList { get; set; }
+        public int Count { get; set; }
+        public List<clsCustomer> ThisAddress { get; set; }
     }
 }
--- Testing1/tstCustomerCollection.cs
+++ Testing1/tstCustomerCollection.cs
@@ -32,5 +32,30 @@ namespace Testing1
             AllCustomers.CustomerList = TestList;
             Assert.AreEqual(AllCustomers.CustomerList, TestList);
         }
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Int32 SomeCount = 0;
+            AllCustomers.Count = SomeCount;
+            Assert.AreEqual(AllCustomers.Count, SomeCount);
+        }
+        [TestMethod]
+        public void ThisAddressPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.AccountVerified = true;
+            TestItem.AccountNo = 1;
+            TestItem.Ballance = 1;
+            TestItem.CustomerName = "johny";
+            TestItem.CustomerEmail = "Heres@johny.com";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.DateOfBirth = Convert.ToDateTime("01/01/2000");
+            
+            AllCustomers.ThisAddress = TestList;
+            Assert.AreEqual(AllCustomers.ThisAddress, TestList);
+        }
     }
 }
